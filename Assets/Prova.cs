d91d337b4a1782621cd77c3a92a25b201f60a04f
adriipereez <adriaperez@ceroca.cat>
2023-10-09 19:02:06 +0200

    limpio

---
+++ Assets/Prova.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prova : MonoBehaviour
+{
+    private float velocity = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moviment
+    }
+}
