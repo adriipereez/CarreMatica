50047023ab6d1406d6f818f8133d13d0baeb84dc
adriipereez <adriaperez@ceroca.cat>
2023-10-23 20:25:05 +0200

    Movimiento coche Y botones
    Movimiento coche con sus limites de las lienas continuas y funcionalidad varios botones del menu

---
+++ Assets/Scripts/BtotonTiendaVovler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtotonTiendaVovler : MonoBehaviour
+{
+    public void cargarEscenaJuego()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/CocheMov.cs
+++ Assets/Scripts/CocheMov.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CocheMov : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidadMovimiento = 5.0f;
+
+    public string Menu;
     void Start()
     {
-        
+       
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+ 
+        float movimientoHorizontal = Input.GetAxis("Horizontal"); 
+
+        float desplazamiento = movimientoHorizontal * velocidadMovimiento * Time.deltaTime;
+
+        transform.Translate(Vector2.right * desplazamiento);
+        Vector2 novaPos = transform.position;
+        novaPos.x = Mathf.Clamp(novaPos.x,-4.25f,4.25f);
+        transform.position = novaPos;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CambiarEscena();
+        }
+    }
+    void CambiarEscena()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
